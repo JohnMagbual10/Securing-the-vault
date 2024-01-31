e6af05b665510e22a464013c6ed6ad7aafb10369
John Magbual <jmagbual@Johns-Air.lan>
2024-01-30 21:36:18 -0800

    vaultcode

---
+++ java.js
@@ -0,0 +1,48 @@
+//1. Display an alert with a secret message
+
+//2. First code will be summed up to 10
+
+//Input firstNumber
+
+let a = 5;
+
+//Input secondNumber
+
+let b = 5;
+
+//Set sum = firstNumber + secondNumber
+
+let sum = a + b;
+
+//Output sum
+
+console.log("sum:", sum);
+
+//3. Second code will be multiplied to 40
+
+let c = 8
+
+// Multiplication
+
+let product = c * a;
+
+// Output product
+
+console.log("Product:", product);
+
+//4. Third code will be divided to remainder 39
+
+//Division (Remainder)
+
+let dividend = 195;
+
+let divisor = 5;
+
+//Output remainder
+
+let remainder = dividend / divisor;
+
+console.log("Remainder:", remainder);
+
+// Vault has been secured
+
